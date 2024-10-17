39fd8088bbdd8269116ce54426501c30a1bfa716
Kenet <Kenneth.nguyen@elev.ga.lbs.se>
2024-10-17 08:59:12 +0200

    aaa

--- SpaceClicker/Assets/Scripts/Click.cs
+++ SpaceClicker/Assets/Scripts/Click.cs
@@ -1,19 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
-
 
 public class Click : MonoBehaviour
 {
-    public TMP_Text ClickText;
-    private int click;
+    public TMP_Text ClickText;         
+    private int click;                 
+    public HeartbeatEffect heartbeat;   
+
+    void Start()
+    {
+        
+        if (heartbeat == null)
+        {
+           
+            heartbeat = GameObject.Find("ClickingButton").GetComponent<HeartbeatEffect>();
+        }
+    }
 
     public void AddClick()
     {
         click++;
         ClickText.text = "Click: " + click;
+
+        
+        if (heartbeat != null)
+        {
+            heartbeat.StartHeartbeat();
+        }
     }
-   
 }

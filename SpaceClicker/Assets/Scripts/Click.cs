6a8f6a0e55ac8ff3b18fb2f8eaf6837fa7a0509f
Kenet <Kenneth.nguyen@elev.ga.lbs.se>
2024-10-17 10:36:24 +0200

    aaaa

--- SpaceClicker/Assets/Scripts/Click.cs
+++ SpaceClicker/Assets/Scripts/Click.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 
 public class Click : MonoBehaviour
 {
-    public TMP_Text ClickText;         
-    private int click;                 
-    public HeartbeatEffect heartbeat;   
+    [SerializeField] public TMP_Text ClickText;
+    [SerializeField] private int click;
+    [SerializeField] public HeartbeatEffect heartbeat;
 
     void Start()
     {
-        
+
         if (heartbeat == null)
         {
-           
+
             heartbeat = GameObject.Find("ClickingButton").GetComponent<HeartbeatEffect>();
         }
     }
@@ -24,7 +24,7 @@ public class Click : MonoBehaviour
         click++;
         ClickText.text = "Click: " + click;
 
-        
+
         if (heartbeat != null)
         {
             heartbeat.StartHeartbeat();

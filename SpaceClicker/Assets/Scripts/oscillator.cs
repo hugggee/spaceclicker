09ed5cf20cf4a3b96cde60c46365734c0bbc9759
Hugo Larsson <hugo.larsson1@learnet.se>
2024-11-03 23:04:58 +0100

    Sista fix, oscillator fixad.

--- SpaceClicker/Assets/Scripts/oscillator.cs
+++ SpaceClicker/Assets/Scripts/oscillator.cs
@@ -1,41 +1,32 @@
-﻿using System;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
 public class OscillatorUI : MonoBehaviour
 {
-    [SerializeField] float radius = 100f;    
-    [SerializeField] float period = 2f;       
+    [SerializeField] float radius = 100f;
+    [SerializeField] float period = 2f;
 
-    private RectTransform rectTransform;      
+    private RectTransform rectTransform;
     private Vector3 startingPosition;
-
-    internal void Initialize(float uniqueOffsetAngle)
+    private float offsetAngle;
+    public void Initialize(float angleOffset)
     {
-        throw new NotImplementedException();
-    }
-
-    void Start()
-    {
-        rectTransform = GetComponent<RectTransform>(); 
-        startingPosition = rectTransform.anchoredPosition; 
+        offsetAngle = angleOffset;
+        rectTransform = GetComponent<RectTransform>();
+        startingPosition = rectTransform.anchoredPosition;
     }
 
     void Update()
     {
-        if (period <= Mathf.Epsilon) return;   
+        if (period <= Mathf.Epsilon) return;
 
-       
-        float cycles = Time.time / period;    
-        const float tau = Mathf.PI * 2f;      
-        float angle = cycles * tau;          
+        float cycles = Time.time / period;
+        const float tau = Mathf.PI * 2f;
+        float angle = cycles * tau + offsetAngle;
 
-        
-        float x = Mathf.Cos(angle) * radius;   
-        float y = Mathf.Sin(angle) * radius;   
+        float x = Mathf.Cos(angle) * radius;
+        float y = Mathf.Sin(angle) * radius;
 
-       
-        Vector3 offset = new Vector3(x, y, 0); 
-        rectTransform.anchoredPosition = startingPosition + offset; 
+        Vector3 offset = new Vector3(x, y, 0);
+        rectTransform.anchoredPosition = startingPosition + offset;
     }
 }
